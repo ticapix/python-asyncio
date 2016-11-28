912257a76321dfc1ed2b4f646920653c57549686
pierre gronlier <ticapix@gmail.com>
2016-11-29 00:12:14 +0100

    add pause, burn_cpu decorator

--- tools.py
+++ tools.py
@@ -27,3 +27,29 @@ def timeme():
             print('{:.42} tooks {:.2f} sec'.format(str(func), ans))
         return wrapper
     return decorator
+
+def _burn_cpu():
+    c = 0
+    for i in range(10**4):
+        c += 1
+
+_unit_sec = timeit.timeit(_burn_cpu, number=5)/5
+
+@timeme()
+def burn_cpu(duration_sec):
+    counter = duration_sec / _unit_sec
+    while counter > 0:
+        _burn_cpu()
+        counter -= 1
+
+def pause():
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            print(func.__doc__)
+            input('\n{:>80}\n'.format('Press <ENTER> to continue'))
+            ans = func(*args, **kwargs)
+            input('\n{:>80}\n'.format('Press <ENTER> to continue'))
+            return ans
+        return wrapper
+    return decorator
